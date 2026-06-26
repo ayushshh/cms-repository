84fa1b1a12f48cf437792f6e1046a9c00cfcf6e3
xfgryv <bs9882074@gmail.com>
2026-06-26 20:02:37 +0530

    added fetchProject using contentful delivery api

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import {useFetchProjects} from  "./fetchProject.jsx"
 
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+  const { loading, projects } = useFetchProjects();
+
+  if(loading){
+    return(
+      <section className='projects'>
+        <h2>Loading...</h2>
+      </section>
+    )
+  }
+
+  return(
+      <section className='projects'>
+        <div className='title'> 
+          <h2>projects</h2>
+          <div className="title-underline"></div>
+        </div>
+        <div className='projects-center'>
+          {
+            projects.map((project) => {
+              const { id, img, url, title} = project;
+              return <a key={id} href={url} target='_blank' rel='noreferrer' className='project'>
+                <img src={img} alt={title} className='img' />
+                <h5>{title}</h5>
+              </a>
+            })
+          }
+        </div>
+      </section>
+    )
 }
 
 export default Projects
